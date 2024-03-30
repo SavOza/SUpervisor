bd1cd148cc01c537c5591baf827bc8ccc576c4d7
SavOza <117209961+SavOza@users.noreply.github.com>
2024-03-31 00:48:55 +0300

    Implemented grid

--- app/src/main/java/com/aozan/courseadvisor/mainactivity/ChooseFilterDialogFragment.kt
+++ app/src/main/java/com/aozan/courseadvisor/mainactivity/ChooseFilterDialogFragment.kt
@@ -1,6 +1,5 @@
 package com.aozan.courseadvisor.mainactivity
 
-import android.app.ActionBar.LayoutParams
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,11 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.aozan.courseadvisor.addfilteractivity.AddFilterActivity
 import com.aozan.courseadvisor.databinding.FragmentChooseFilterDialogBinding
-import com.aozan.courseadvisor.gradreqactivity.GradReqActivity
 
 class ChooseFilterDialogFragment : DialogFragment() {
     private lateinit var binding: FragmentChooseFilterDialogBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentChooseFilterDialogBinding.inflate(LayoutInflater.from(context))
 
         binding.icAdd.setOnClickListener{
@@ -23,10 +21,10 @@ class ChooseFilterDialogFragment : DialogFragment() {
 
         return binding.root
     }
-    
+
     override fun onResume() {
         super.onResume()
-        var params = dialog?.window?.attributes
+        val params = dialog?.window?.attributes
 
         if (params != null) {
             params.width = 1000
